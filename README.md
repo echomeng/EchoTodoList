725ef7367064969d7a0c7ee5402f906806835b8b
echomeng <mengxiangxxa@163.com>
2018-12-18 20:15:11 +0800

    todolist

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EchoTodoList
 一个微信小程序的练习，目的在于熟悉链式编程
+
+
+依照[mistory/todolist](https://github.com/mistory/todolist)写的小程序，完成了list和log两个页面，能够实现列表和日志的本地存储。
--- TodoList/pages/index/index.js
+++ TodoList/pages/index/index.js
@@ -1,54 +1,154 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    todos: [],
+    input: "",
+    leftCount: 0,
+    allCompleted: false,
+    logs: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  onShow: function() {
+    var logs = wx.getStorageSync('todo_logs');
+    var todos = wx.getStorageSync('todo_list');
+    var leftCount = wx.getStorageSync('left_count')
+    if (logs) {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+        logs: logs
+      });
+    }
+    if (todos) {
+      this.setData({
+        todos:todos,
+      });
+    }
+    if (leftCount > 0) {
+      this.setData({
+        leftCount: leftCount,
+      });
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  save: function () {
+    wx.setStorageSync('todo_list', this.data.todos);
+    wx.setStorageSync('todo_logs', this.data.logs);
+    wx.setStorageSync('left_count', this.data.leftCount);
+  },
+  inputChange: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      input: e.detail.value
     })
+  },
+  inputConfirm: function (e) {
+    var input = this.data.input.trim();
+    if (!input) return;
+    var todos = this.data.todos?this.data.todos:[];
+    var leftCount = this.data.leftCount;
+    leftCount++;
+    todos.push({
+      name: input,
+      completed: false
+    });
+    var log = {
+      name: input,
+      behave: 'Add',
+      time: new Date()
+    }
+    var logs = this.data.logs;
+    logs.push(log);
+    this.setData({
+      todos: todos,
+      input: '',
+      leftCount: leftCount,
+      logs: logs,
+    });
+    this.save();
+  },
+  removeClick: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var todos = this.data.todos;
+    var logs = this.data.logs;
+    var removeItem = todos.splice(index, 1)[0];
+    var leftCount = this.data.leftCount;
+    if (!removeItem.completed && leftCount > 0) {
+      leftCount--;
+    }
+    if (removeItem) {
+      logs.push({
+        name: removeItem.name,
+        behave: 'Remove',
+        time: new Data()
+      });
+      this.setData({
+        todos: todos,
+        leftCount: leftCount,
+        logs: logs,
+      })
+      this.save();
+    }
+  },
+  handleTodo: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var todos = this.data.todos;
+    var logs = this.data.logs;
+    todos[index].completed = !todos[index].completed;
+    var leftCount = this.data.leftCount;
+    if (todos[index].completed) {
+      if(leftCount > 0) leftCount--;
+      logs.push({
+        name: todos[index].name,
+        behave: 'Finish',
+        time: new Date()
+      });
+    }
+    else {
+      leftCount++;
+      logs.push({
+        name: todos[index].name,
+        behave: 'Restart',
+        time: new Date()
+      });
+    }
+    this.setData({
+      todos: todos,
+      leftCount: leftCount,
+      logs: logs,
+    });
+    this.save();
+  },
+  clearCompleted: function (e) {
+    var todos = this.data.todos;
+    var logs = this.data.logs;
+    var remainTodos = [];
+    for (let i = 0; i < todos.length; i++) {
+      if (!todos[i].completed) {
+        remainTodos.push(todos[i]);
+      } else {
+        logs.push({
+          name: todos[index].name,
+          behave: 'Finish',
+          time: new Data()
+        })
+      }
+    }
+    var leftCount = remainTodos.length;
+    this.setData({
+      todos: remainTodos,
+      logs: logs,
+      leftCount: leftCount,
+    });
+    this.save();
+  },
+  toggleAll: function (e) {
+    this.data.completed = !this.data.completed;
+    var todos = this.data.todos;
+    for (let i = 0; i < todos.length; i++) {
+      if (this.data.completed) {
+        todos[i].completed = true;
+      } else {
+        todos[i].completed = false;
+      }
+    }
+    this.setData({
+      todos: todos,
+    });
+    this.save();
   }
 })
